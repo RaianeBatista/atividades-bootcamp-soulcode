ab42e0a2b7a6795910e84dfb075242567166b158
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-07-08 14:15:31 -0300

    feta: add content JavaScript

---
+++ JS/aula-12/operadores/operadores.js
@@ -0,0 +1,31 @@
+//console.log("Ola mundo!");
+
+// Para abrir o terminal control + j
+
+// Atalho para a pasta desejada no terminal
+//  cd js/aula-12/operadores e
+// com o botão direito Open in Integrated Terminal, na pasta desejada,
+// ja abri direto no terminal o atalho desejada
+// para executar o arquivo pelo terminal:
+// C:\Bootcamp_SoulCode\web-dev-studies\JS\aula-12\operadores>node operadores.js
+//Ola mundo!
+// da pra arrastar e soltar no terminal tbm gente
+//'use strict' obriga a ter o ponto a virgula ; no final
+// Para limpar o terminal digite o cls
+let nome = "Raiane Batista";
+let idade = 36;
+
+nome = "José Davi";
+
+//console.log(nome, idade);
+//console.log(idade)
+
+const PI = 3.14;
+
+// PI = 3.10 // erro
+
+//console.log(PI);
+
+// Tipagem
+let hoje = "08/07/2025";
+console.log(hoje);
---
+++ JS/aula-12/script.js
@@ -0,0 +1,14 @@
+console.log("Script externo.");
+
+// Padrões de nomenclaturas e Convenção
+// RaianeBatista -> PascalCase
+// raiane_batista -> snake case
+// raianeBatista -> Camel Case - Convenção JavaScript
+
+let raiane_batista
+let $ = 2
+//let 4teste = 2
+let teste4 = 7
+let AMOR = 'JavaScript'
+
+
