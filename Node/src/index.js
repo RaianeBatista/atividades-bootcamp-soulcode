5dd0e2f86be5524630161c9c1b823ec3244e7bc3
RaianeBatista <202311169804@alunos.estacio.br>
2025-07-25 11:31:49 -0300

    feat: add lib .env

--- Node/src/index.js
+++ Node/src/index.js
@@ -1,10 +1,12 @@
+import "dotenv/config";
 import express from "express";
 import produtosRouter from "./routers/produtosRouter.js";
 import usuariosRouter from "./routers/usuariosRouter.js";
 
 const server = express();
 server.use(express.json());
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 server.use(produtosRouter);
 server.use(usuariosRouter);
--- Node/src/services/usuariosService.js
+++ Node/src/services/usuariosService.js
@@ -40,10 +40,25 @@ async function remove(id) {
   const docRef = db.collection("usuarios").doc(id);
   const doc = await docRef.get();
 
-  if (doc.exists) {
-    return true;
-  }
-  return false;
+  if (!doc.exists) 
+    return false;
+  
+  await docRef.delete()
+  return true
 }
 
 export { findAll, findById, save, remove };
+
+
+// solução alternativa
+// async function remove(id) {
+//     const docRef = db.collection("usuarios").doc(id);
+//     const doc = await docRef.get();
+
+//     if (doc.exists) {
+//         await docRef.delete();
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
